9391a09327c6f5df8216935e126464b8973caf3a
Lesson Koirala <58141138+LissanKoirala@users.noreply.github.com>
2024-11-20 16:45:03 +0000

    stuff

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,31 @@
-import flask
+from flask import Flask, request, jsonify, render_template
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Home route
+@app.route('/')
+def home():
+    return "<h1>Hello world</h1>"
+
+
+
+# Example route with query parameters
+@app.route('/translate')
+def greet():
+    from_language = request.args.get("from_lang")
+    to_language = request.args.get("to_lang")
+    text = request.args.get("text")
+
+    # translate()
+    
+    return 
+
+# Example route using a template
+@app.route('/template')
+def template_example():
+    return render_template('index.html', name="Flask")
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ to_speech.py
@@ -0,0 +1,38 @@
+import os
+from google.cloud import texttospeech
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'culture-hack.json'
+
+
+def speech_to_file(text_block):
+
+
+    client = texttospeech.TextToSpeechClient()
+
+
+    synthesis_input = texttospeech.SynthesisInput(text= text_block)
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-GB", 
+        name= 'en-GB-Journey-D'
+        # name= 'en-GB-News-K'
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3,
+        effects_profile_id=['handset-class-device'],
+        # speaking_rate=1.0,
+        # pitch=1
+    )
+
+    response = client.synthesize_speech(
+        input=synthesis_input, 
+        voice=voice, 
+        audio_config=audio_config
+    )
+
+    with open("output.mp3", "wb") as output:
+        output.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+
